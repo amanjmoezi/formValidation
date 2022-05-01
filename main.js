4815f120f9bf7ab8b8628213e34474cea8a98c2e
AMANJMOEZI <79466023+amanjmoezi@users.noreply.github.com>
2022-05-01 21:33:06 +0430

    add htmlElement in var for select elements

---
+++ main.js
@@ -0,0 +1,5 @@
+const FormDeta = document.querySelector("form");
+const inputEmail = document.querySelector("input.user-input");
+const emailMsg = document.querySelector(".username-msg");
+const inputPassword = document.querySelector("input.pass-input");
+const passwordMsg = document.querySelector(".password-msg");
