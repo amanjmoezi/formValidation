05026116ed797331aad70263ac281225e956e8a3
AMANJMOEZI <79466023+amanjmoezi@users.noreply.github.com>
2022-05-01 21:37:35 +0430

    add addeventlistener for submit form

--- main.js
+++ main.js
@@ -3,3 +3,9 @@ const inputEmail = document.querySelector("input.user-input");
 const emailMsg = document.querySelector(".username-msg");
 const inputPassword = document.querySelector("input.pass-input");
 const passwordMsg = document.querySelector(".password-msg");
+let IfloginTrue=false;
+FormDeta.addEventListener("submit",submitForm)
+function submitForm(e){
+    e.preventDefault()
+
+}
